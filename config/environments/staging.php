7f17d04cd60a74b8aa883df4b53b00eb539530ba
Brando Meniconi <meniconi.brando@gmail.com>
2015-04-15 16:06:18 +0200

    mod staging env

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -5,10 +5,14 @@ define('DB_USER', getenv('DB_USER'));
 define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 
-define('WP_HOME', getenv('WP_HOME'));
-define('WP_SITEURL', getenv('WP_SITEURL'));
-
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
+
+$batcache = [
+  'seconds'=>0,
+  'max_age'=>30*60, // 30 minutes
+  'group'=>'batcache_'.WP_CACHE_KEY_SALT,
+  'debug'=>true
+];
