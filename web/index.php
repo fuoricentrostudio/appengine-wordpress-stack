82d16022d59565747c0e7b10465b6af5aad1319c
Brando Meniconi <meniconi.brando@gmail.com>
2015-06-24 16:22:34 +0200

    dirty fix for attachments pages: disabled

--- web/index.php
+++ web/index.php
@@ -2,4 +2,12 @@
 
 // WordPress view bootstrapper
 define('WP_USE_THEMES', true);
+
+if(isset($_GET['attachment_id'])){
+    header('HTTP/1.1 503 Service Temporarily Unavailable');
+    header('Status: 503 Service Temporarily Unavailable');
+    header('Retry-After: 36000');//300 seconds    
+    exit();
+}
+
 require(__DIR__ . '/wp/wp-blog-header.php');
