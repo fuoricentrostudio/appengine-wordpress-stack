0a63647ba283244583f174ee462c0b5b34575618
Brando Meniconi <meniconi.brando@gmail.com>
2015-06-24 16:28:16 +0200

    disabled theme version update fix, causes widget disabled

--- web/app/mu-plugins/register-theme-directory.php
+++ web/app/mu-plugins/register-theme-directory.php
@@ -12,9 +12,3 @@ License:      MIT License
 if (!defined('WP_DEFAULT_THEME')) {
   register_theme_directory(ABSPATH . 'wp-content/themes');
 }
-
-if (get_site_transient( 'appengine_version_id' ) !== getenv('CURRENT_VERSION_ID') ) {
-    delete_site_transient('theme_roots');
-    switch_theme(get_stylesheet());
-    set_site_transient( 'appengine_version_id', getenv('CURRENT_VERSION_ID') );
-}
