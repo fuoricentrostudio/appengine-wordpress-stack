440e9af242361e84d8da74cd347791d2942aac22
Brando Meniconi <meniconi.brando@gmail.com>
2015-07-31 12:45:50 +0200

    force debug defins off

--- config/environments/production.php
+++ config/environments/production.php
@@ -6,9 +6,13 @@ define('DB_PASSWORD', getenv('DB_PASSWORD'));
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
 
 ini_set('display_errors', 0);
+define('SAVEQUERIES', false);
+define('WP_DEBUG', false);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
+
+define('WP_POST_REVISIONS', 3);
 
 $batcache = [
   'seconds'=>0,
